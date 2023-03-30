684141939c704825c88764557a5118f83cb0d108
KamDaDaddy <97680603+Ahacker1208@users.noreply.github.com>
2023-03-30 12:16:46 -0400

    5.2 done

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,6 +10,10 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,7 @@ public class Target : MonoBehaviour
      targetRb.AddTorque(RandomTorque(), RandomTorque(),
      RandomTorque(), ForceMode.Impulse);
      transform.position = RandomSpawnPos();
-
+     gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     
     Vector3 RandomForce() 
@@ -48,5 +52,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, 
+        explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 }
